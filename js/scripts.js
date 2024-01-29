b88b2af8c41efac46014e9bff3c8e864a117ac52
Nikita <32540777+nivanown@users.noreply.github.com>
2024-01-29 20:48:57 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -138,3 +138,45 @@ $('.gl-accordion__top-panel').click(function() {
   $(this).next().slideToggle(200);
   $(this).toggleClass('show', 200);
 });
+
+/*- main-navi -*/
+var last_id;
+var $top_menu = $('.price-info__nav');
+var menu_height = $top_menu.outerHeight(true);
+var $menu_items = $top_menu.find('a');
+var $scroll_items = $menu_items.map(function(){
+  var item = $($(this).attr('href'));
+  if (item.length){
+    return item;
+  }
+});
+
+$menu_items.click(function(e){
+  var href = $(this).attr('href'),
+  offset_top = href === '#' ? 0 : $(href).offset().top - menu_height;
+  $('html, body').stop().animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 700);
+  e.preventDefault();
+});
+
+$(window).scroll(function(){
+  var from_top = $(this).scrollTop() + menu_height;
+  var mar = parseInt($top_menu.css('margin-bottom'));
+  var cur = $scroll_items.map(function(){
+    if ($(this).offset().top < from_top + mar){
+      return this;
+    }
+  });
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : '';
+  if (last_id !== id){
+    last_id = id;
+    $menu_items.parent()
+      .removeClass('active')
+      .end()
+      .filter("[href='#" + id + "']")
+      .parent()
+      .addClass('active');
+  }
+});
